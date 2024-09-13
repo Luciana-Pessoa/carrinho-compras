91bf7c163b5ed928fa19339a7a1c0d3628bdfec5
lu9932323258@gmail.com <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-09-13 16:59:12 -0300

    add

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 function adicionar(){
 //recuperar valores nome do produto, quantidade e valor
-let nomeProduto = document.getElementById('produto');
-let quantidade = document.getElementById('quantidade');
-
-//teste para ver se esta chamando a classe correta
-// alert(nomeProduto.value);
-
+let Produto = document.getElementById('produto').value;
+let nomeProduto = Produto.split('-')[0];
+let valorUnitario = Produto.split('R$')[1];
+let quantidade = document.getElementById('quantidade').value;
 
 //calcular o preço, o nosso subtotal
+let preco = quantidade * valorUnitario;
+
+
 //adicionar o produto no carrinho
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML = carrinho.innerHTML +`<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">${preco}</span>
+        </section>`;
+
+
+
+
 //atualizar o valor total da compra
 
 
