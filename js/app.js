aa2d39bfc35b77f902c07b4647124ab96f13ce67
lu9932323258@gmail.com <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-09-13 15:57:23 -0300

    chamando as classes corretas

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function adicionar(){
+//recuperar valores nome do produto, quantidade e valor
+let nomeProduto = document.getElementById('produto');
+let quantidade = document.getElementById('quantidade');
+
+//teste para ver se esta chamando a classe correta
+// alert(nomeProduto.value);
+
+
+//calcular o preço, o nosso subtotal
+//adicionar o produto no carrinho
+//atualizar o valor total da compra
+
+
+}
+
+function limpar (){
+
+
+}
